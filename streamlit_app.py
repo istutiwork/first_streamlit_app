7f2b36248638818975e1242eb42be440be38cc3d
istutiwork <110223205+istutiwork@users.noreply.github.com>
2022-07-29 13:43:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.text("🥣 Omega 3 and blueberry Oatmeal")
 streamlit.text("🥗 Kale,Spinach and Rocket Smoothie")
 streamlit.text("🐔 Hard-boiled Free-Range egg")
 streamlit.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
