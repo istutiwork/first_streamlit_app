fbc115d023648ded0573d47e365bbc8c2375dcff
istutiwork <110223205+istutiwork@users.noreply.github.com>
2022-07-29 13:32:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My parents new healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and blueberry Oatmeal")
+streamlit.text("Kale,Spinach and Rocket Smoothie")
+streamlit.text("Hard-boiled free-range egg")
